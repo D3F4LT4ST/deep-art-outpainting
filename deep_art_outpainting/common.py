8d1d9c18127f56b048cfa4bd741779c6bc78f25a
Andrew Stepura <andstepura@gmail.com>
2023-03-30 16:49:35 -0400

    Added super resolution dataset preparation code

---
+++ deep_art_outpainting/common.py
@@ -0,0 +1,3 @@
+import os
+
+DATA_PATH = os.path.abspath(os.path.abspath(__file__) + '../../../data')
---
+++ deep_art_outpainting/super_resolution/data_preparation.py
@@ -0,0 +1,49 @@
+import os
+import cv2 as cv
+from ..common import DATA_PATH
+
+def prepare_sr_dataset(
+        hr_size: int=2048, 
+        lr_factor: int=4 
+    ):
+    '''
+    Prepares WikiArt super resolution dataset by creating high and low resolution versions of the images.
+
+    Args:
+        hr_size: high resolution image size (smallest dimension)
+        lr_factor: low resolution downscaling factor
+    '''
+    for subset in ['train', 'valid', 'test']:
+
+        orig_subset_path = os.path.abspath(DATA_PATH + f'/WikiArtSR/{subset}')
+        hr_subset_path = os.path.abspath(DATA_PATH + f'/WikiArtSR_HR/{subset}')
+        lr_subset_path = os.path.abspath(DATA_PATH + f'/WikiArtSR_LR/{subset}')
+
+        os.makedirs(hr_subset_path, exist_ok=True)
+        os.makedirs(lr_subset_path, exist_ok=True)
+
+        for fname in filter(lambda fname: not fname.startswith('.'), os.listdir(orig_subset_path)):
+            
+            orig_img = cv.imread(os.path.abspath(orig_subset_path + f'/{fname}'))
+
+            height, width, _ = orig_img.shape
+            aspect_ratio = width / height
+
+            smallest_dim = min(width, height)
+            if smallest_dim == height:
+                height_hr = hr_size
+                width_hr = int(hr_size * aspect_ratio)
+            else:
+                width_hr = hr_size
+                height_hr = int(hr_size / aspect_ratio)
+                
+            hr_interp = cv.INTER_AREA if smallest_dim > hr_size else cv.INTER_CUBIC
+            img_hr = cv.resize(orig_img, (width_hr, height_hr), interpolation=hr_interp)
+
+            height_lr = height_hr // lr_factor
+            width_lr = width_hr // lr_factor
+
+            img_lr = cv.resize(img_hr, (width_lr, height_lr), interpolation=cv.INTER_AREA)
+
+            cv.imwrite(os.path.abspath(hr_subset_path + f'/{fname.split(".")[0]}.jpg'), img_hr)
+            cv.imwrite(os.path.abspath(lr_subset_path + f'/{fname.split(".")[0]}.jpg'), img_lr)
