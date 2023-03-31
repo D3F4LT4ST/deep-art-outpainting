8dde465d951572b2cf67b083d07db0b6141536c1
Andrew Stepura <andstepura@gmail.com>
2023-03-30 20:32:40 -0400

    Added script for fetching data

---
+++ scripts/fetch_data.sh
@@ -0,0 +1,5 @@
+# WikiArtSR
+wget 'https://docs.google.com/uc?export=download&id=1YYO1eIJyfpIchl-0Z8owbO1SWVWhjwaX&confirm=t' -O WikiArtSR.zip
+unzip WikiArtSR.zip
+rm WikiArtSR.zip
+mv WikiArtSR ../data/WikiArtSR
