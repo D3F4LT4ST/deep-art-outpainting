d9406624f9fb0126709184e45fb6669179f548c1
Andrew Stepura <andstepura@gmail.com>
2023-03-31 16:23:33 -0400

    Added requirements, updated gitignore

--- scripts/fetch_weights.sh
+++ scripts/fetch_weights.sh
@@ -3,4 +3,5 @@ wget 'https://public-asai-dl-models.s3.eu-central-1.amazonaws.com/ISR/rdn-C6-D20
 wget 'https://public-asai-dl-models.s3.eu-central-1.amazonaws.com/ISR/rdn-C3-D10-G64-G064-x2/PSNR-driven/rdn-C3-D10-G64-G064-x2_PSNR_epoch134.hdf5'
 wget 'https://public-asai-dl-models.s3.eu-central-1.amazonaws.com/ISR/rdn-C6-D20-G64-G064-x2/ArtefactCancelling/rdn-C6-D20-G64-G064-x2_ArtefactCancelling_epoch219.hdf5'
 wget 'https://public-asai-dl-models.s3.eu-central-1.amazonaws.com/ISR/rrdn-C4-D3-G32-G032-T10-x4-GANS/rrdn-C4-D3-G32-G032-T10-x4_epoch299.hdf5'
+mkdir ../weights/SR
 mv *.hdf5 ../weights/SR
