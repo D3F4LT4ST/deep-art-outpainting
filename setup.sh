0fa3ea62755413aa1e244dc97cf9c9afeb45e9a0
Andrew Stepura <andstepura@gmail.com>
2023-04-05 15:46:05 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 wget 'https://docs.google.com/uc?export=download&id=1YYO1eIJyfpIchl-0Z8owbO1SWVWhjwaX&confirm=t' -O WikiArtSR.zip
 unzip WikiArtSR.zip
 rm WikiArtSR.zip
+mkdir -p data
 mv WikiArtSR data/WikiArtSR
 
 # Fetch weights
 wget 'https://public-asai-dl-models.s3.eu-central-1.amazonaws.com/ISR/rrdn-C4-D3-G32-G032-T10-x4-GANS/rrdn-C4-D3-G32-G032-T10-x4_epoch299.hdf5'
-mkdir weights/SR
+mkdir -p weights/SR
 mv *.hdf5 weights/SR
 
 wget 'https://docs.google.com/uc?export=download&id=19qzrIGWUW3eu5troIjHzhCIbTZONSRjX&confirm=t' -O 4xPSNR.pth
